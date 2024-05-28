f0d1d8dfad32d1f6ef577deec429bf19462725e5
sanpope <166558988+sanpope@users.noreply.github.com>
2024-05-28 15:41:04 -0500

    Update App.js
    Traducción de mensaje al español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edita <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
           className="App-link"
